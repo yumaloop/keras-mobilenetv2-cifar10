f8297268ec94ec37955f092269d343e67e40746d
yumaloop <yumaloop@gmail.com>
2020-02-21 06:31:58 +0000

    Fix bug

--- run.sh
+++
@@ -1,5 +0,0 @@
-python3 run.py \
---trans_learn \
---weights_path ./log/lenet5/base/best_weights.h5
-
-# python3 run.py --model lenet5 --type qc --trans_learn --weights_path ./log/lenet5/base/best_weights.h5
